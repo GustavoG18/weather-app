634efff8edf2eaff3c4bc8abfc3f8be686818844
GustavoG18 <gguerrerof98@gmail.com>
2023-11-20 02:30:03 -0500

    Refactor header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 import { GoogleApiService } from 'src/app/services/google-api.service'
 import { Options } from 'ngx-google-places-autocomplete/objects/options/options'
 import { Address } from 'ngx-google-places-autocomplete/objects/address'
@@ -12,32 +12,19 @@ import { selectListWeather } from 'src/app/state/selectors/weather.selector'
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   public options!: Options
   public handleChange!: (address: Address) => void
 
   public weather$: Observable<WeatherModel> = new Observable()
-  public firstCall: boolean = true
 
   constructor (
     public readonly googleApiService: GoogleApiService,
     public readonly store: Store<AppState>
   ) {
-    this.options = googleApiService.options
-    this.handleChange = (address: Address) => googleApiService.handleChange(address)
-  }
-
-  ngOnInit (): void {
     this.weather$ = this.store.select(selectListWeather)
-    this.weather$.subscribe((response: WeatherModel) => {
-      console.log({ response })
-      this.isFirstCall(response)
-    })
-  }
-
-  isFirstCall (currentWeather: WeatherModel): void {
-    if (currentWeather.weatherByDays.length > 0) {
-      this.firstCall = false
-    }
+    this.options = googleApiService.options
+    this.handleChange = (address: Address) =>
+      googleApiService.handleChange(address)
   }
 }
